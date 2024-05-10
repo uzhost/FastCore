67455a039f08114bde93684c28ffb058c62683a4
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2024-05-10 23:03:05 +0500

    Update settings.php

--- pages/user/settings.php
+++ pages/user/settings.php
@@ -1,13 +1,13 @@
-<?php if(!defined('FastCore')){exit('Opss!');}
+<?php if(!defined('FastCore')){exit('Oops!');}
 
-# Заголовок
-$opt['title'] = 'Настройки';
+# Title
+$opt['title'] = 'Settings';
 ?>
 
 <div class="row">
 <div class="col-lg-6">
 <div class="card">
-<div class="card-header">Поменять пароль</div>
+<div class="card-header">Change Password</div>
 <div class="p-3">
 <?
 if(isset($_POST['new_pass'])){
@@ -16,17 +16,17 @@ $pass_new = $func->FPass($_POST['pass_new']);
 if($pass !== false AND strtolower($pass) == strtolower($user['pass'])){
 	if($pass_new !== false){
 		$db->query("UPDATE db_users SET pass = '$pass_new' WHERE id = '$uid'");        
-		echo '<div class="alert alert-success">Пароль успешно изменен!</div>';
-	}else echo '<div class="alert alert-warning">Новый пароль имеет неверный формат!</div>';
-}else echo '<div class="alert alert-danger">Старый пароль заполнен неверно!</div>';
+		echo '<div class="alert alert-success">Password changed successfully!</div>';
+	}else echo '<div class="alert alert-warning">New password has an invalid format!</div>';
+}else echo '<div class="alert alert-danger">Old password is incorrect!</div>';
 }
 ?>
 
 <form action="" method="POST">
-<input type="password" class="form-control" name="pass" placeholder="Старый пароль">
-<input type="password" class="form-control mt-2" name="pass_new" placeholder="Новый пароль">
+<input type="password" class="form-control" name="pass" placeholder="Old Password">
+<input type="password" class="form-control mt-2" name="pass_new" placeholder="New Password">
 <center>
-<button class="btn btn-success mt-2" name="new_pass" type="submit">СМЕНИТЬ ПАРОЛЬ</button>
+<button class="btn btn-success mt-2" name="new_pass" type="submit">CHANGE PASSWORD</button>
 </center>
 </form>
 </div>
@@ -35,16 +35,16 @@ if($pass !== false AND strtolower($pass) == strtolower($user['pass'])){
 
 <div class="col-lg-6">
 <div class="card">
-<div class="card-header">Платежные реквизиты</div>
+<div class="card-header">Payment Details</div>
 <div class="p-3">
 <?php
-# Класс кошельков
+# Wallet class
 $wallet = new wallets();
 
-# Кошельки и платежный пароль
+# Wallets and payment password
 $ps = $db->query('SELECT * FROM db_purse WHERE id = ?',$uid)->fetchArray();
 
-# Привязать кошелек
+# Bind Wallet
 if(isset($_POST['save_wallet'])) {
 
 $payeer = $wallet->payeer_wallet($_POST['payeer']);
@@ -56,8 +56,8 @@ if($payeer !== false) {
 $ok = $db->query('SELECT * FROM db_purse WHERE payeer = ?',array($payeer))->numRows();
 	if ($ok == 0 && $ps['payeer'] == '0'){
 		$db->query('UPDATE db_purse SET payeer = ? WHERE id = ?',array($payeer, $uid));
-		$save_p[]='Кошелек PAYEER сохранен';
-	} else { $err[] = 'Такой кошелек занят!'; }
+		$save_p[]='PAYEER Wallet saved';
+	} else { $err[] = 'This wallet is already in use!'; }
 }
 
 # QIWI
@@ -65,8 +65,8 @@ if($qiwi !== false) {
 $ok = $db->query('SELECT * FROM db_purse WHERE qiwi = ?',array($qiwi))->numRows();
 	if ($ok == 0 && $ps['qiwi'] == '0'){
 		$db->query('UPDATE db_purse SET qiwi = ? WHERE id = ?',array($qiwi, $uid));
-		$save_p[]='Кошелек QIWI сохранен';
-	} else { $err[] = 'Такой кошелек занят!'; }
+		$save_p[]='QIWI Wallet saved';
+	} else { $err[] = 'This wallet is already in use!'; }
 }
 
 # YANDEX
@@ -74,8 +74,8 @@ if($yandex !== false) {
 $ok = $db->query('SELECT * FROM db_purse WHERE yandex = ?',array($yandex))->numRows();
 	if ($ok == 0 && $ps['yandex'] == '0'){
 		$db->query('UPDATE db_purse SET yandex = ? WHERE id = ?',array($yandex, $uid));
-		$save_p[]='Кошелек YANDEX сохранен';
-	} else { $err[] = 'Такой кошелек занят!'; }
+		$save_p[]='YANDEX Wallet saved';
+	} else { $err[] = 'This wallet is already in use!'; }
 }
 
 # Errors
@@ -87,7 +87,7 @@ if (!empty($err)) {
 if (!empty($save_p)) {
 	echo '<div class="alert alert-success">'.array_shift($save_p).'</div>'; }
 else {
-	echo '<div class="alert alert-danger">Кошелек заполнен неверно!</div>'; }
+	echo '<div class="alert alert-danger">Wallet entered incorrectly!</div>'; }
 }
 ?>
 
@@ -97,38 +97,38 @@ if ($ps['payeer'] == '0') {
 ?>
 <div class="input-group mb-2">
 	<div class="input-group-prepend"><span class="input-group-text">PAYEER</span></div>
-	<input class="form-control" type="text" name="payeer" placeholder="Пример P111111111" value=""/>
+	<input class="form-control" type="text" name="payeer" placeholder="Example P111111111" value=""/>
 </div>
 <?php
 } else {
-	echo '<div class="alert alert-info p-2"><b>ВАШ PAYEER:</b> '.$ps['payeer'].'</div>';
+	echo '<div class="alert alert-info p-2"><b>YOUR PAYEER:</b> '.$ps['payeer'].'</div>';
 }
 if ($ps['qiwi'] == '0') {
 ?>
 <div class="input-group mb-2">
 	<div class="input-group-prepend"><span class="input-group-text">QIWI</span></div>
-	<input class="form-control" type="text" name="qiwi" placeholder="Пример +79201234567" value="" />
+	<input class="form-control" type="text" name="qiwi" placeholder="Example +79201234567" value="" />
 </div>
 <?php
 } else {
-	echo '<div class="alert alert-info p-2"><b>ВАШ QIWI:</b> '.$ps['qiwi'].'</div>';
+	echo '<div class="alert alert-info p-2"><b>YOUR QIWI:</b> '.$ps['qiwi'].'</div>';
 }
 if ($ps['yandex'] == '0') {
 ?>
 <div class="input-group">
-	<div class="input-group-prepend"><span class="input-group-text">ЯНДЕКС</span></div>
-	<input class="form-control" type="text" name="yandex" placeholder="Пример 40012345600000" value="" />
+	<div class="input-group-prepend"><span class="input-group-text">YANDEX</span></div>
+	<input class="form-control" type="text" name="yandex" placeholder="Example 40012345600000" value="" />
 </div>
 <?php
 } else {
-	echo '<div class="alert alert-info p-2"><b>ВАШ ЯНДЕКС:</b> '.$ps['yandex'].'</div>';
+	echo '<div class="alert alert-info p-2"><b>YOUR YANDEX:</b> '.$ps['yandex'].'</div>';
 }
 ?>
 <?php
 If ($ps['payeer'] && $ps['qiwi'] && $ps['yandex'] > 0) { } else {
 ?>
 <center>
-<button class="btn btn-success mt-2" name="save_wallet" type="submit">Сохранить реквизиты</button>
+<button class="btn btn-success mt-2" name="save_wallet" type="submit">Save Details</button>
 </center>
 <? } ?>
 
