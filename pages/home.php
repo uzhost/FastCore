a0e5156ea54b9d021c725c2e7565cf81714efb32
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2024-05-11 00:54:46 +0500

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -22,20 +22,20 @@ $stats = $db->query("SELECT * FROM db_stats WHERE id = '1'")->fetchArray();
 <div class="jumbotron mb-2">
     <div class="container">
         <h1 class="display-4"><b><?=$config->sitename;?></b></h1>
-        <p class="lead">Superfast script for creating sites on PHP 8</p>
+        <p class="lead" style="font-size: 1.5rem;">Superfast script for creating sites on PHP 8</p>
         <hr class="my-4">
         <div class="row mb-4">
             <div class="col-lg-2 col-md-3">
                 <p><small class="fw-bold">Script</small></p>
-                <p class="fs-8">FastCore v0.8.1</p>
+                <p class="fs-5">FastCore v0.8.1</p>
             </div>
             <div class="col-lg-2 col-md-3">
                 <p><small class="fw-bold">Release</small></p>
-                <p class="fs-8">10.05.2024</p>
+                <p class="fs-5">10.05.2024</p>
             </div>
             <div class="col-lg-6 col-md-6">
                 <p><small class="fw-bold">Requirements</small></p>
-                <p class="fs-8">PHP 8.0, Mysqli, Utf-8, Apache, FastCGI</p>
+                <p class="fs-5">PHP 8.0, Mysqli, Utf-8, Apache, FastCGI</p>
             </div>
         </div>
         <div><a href="https://github.com/uzhost/fastcore" class="btn btn-lg btn-primary">Download</a></div>
@@ -44,46 +44,47 @@ $stats = $db->query("SELECT * FROM db_stats WHERE id = '1'")->fetchArray();
 
 
 
+
 <div class="row text-center text-uppercase mb-3 mt-3">
     <div class="col-md-3">
         <div class="p-2 border">
-            <i class="fa fa-users" style="font-size: 3rem;"></i>
-            <div class="mt-2"><b><?=$stats['users'];?> people</b></div>
-            <div>Users</div>
+            <i class="fa fa-users" style="font-size: 3.5rem;"></i>
+            <div class="mt-2" style="font-size: 1.1rem;"><b><?=$stats['users'];?> people</b></div>
+            <div style="font-size: 1.1rem;">Users</div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="p-2 border">
-            <i class="fa fa-briefcase" style="font-size: 3rem;"></i>
-            <div class="mt-2"><b><?=$stats['inserts'];?> rub.</b></div>
-            <div>Top-up</div>
+            <i class="fa fa-briefcase" style="font-size: 3.5rem;"></i>
+            <div class="mt-2" style="font-size: 1.1rem;"><b><?=$stats['inserts'];?> rub.</b></div>
+            <div style="font-size: 1.1rem;">Top-up</div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="p-2 border">
-            <i class="fa fa-ruble-sign" style="font-size: 3rem;"></i>
-            <div class="mt-2"><b><?=$stats['payments'];?> rub.</b></div>
-            <div>Paid out</div>
+            <i class="fa fa-ruble-sign" style="font-size: 3.5rem;"></i>
+            <div class="mt-2" style="font-size: 1.1rem;"><b><?=$stats['payments'];?> rub.</b></div>
+            <div style="font-size: 1.1rem;">Paid out</div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="p-2 border">
-            <i class="fa fa-university" style="font-size: 3rem;"></i>
-            <div class="mt-2"><b><?=intval(((time() - $config->start_time) / 86400 ) +1); ?></b></div>
-            <div>Days in operation</div>
+            <i class="fa fa-university" style="font-size: 3.5rem;"></i>
+            <div class="mt-2" style="font-size: 1.1rem;"><b><?=intval(((time() - $config->start_time) / 86400 ) +1); ?></b></div>
+            <div style="font-size: 1.1rem;">Days in operation</div>
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-lg-8">
-        <div class="p-3 border">
+        <div class="p-3 border" style="font-size: 1.1rem;">
             <h4>About the Script</h4>
             <p>This script is designed for creating websites of any complexity. It includes basic functionality necessary for initial use, as well as familiar sections from predecessors. Now everything works much faster and safer. We have made this script as simple and convenient as possible, and scalable. This version of the script requires further development and will undergo some changes. The script cannot be sold in its pure form as it is public. If you have ideas and understand coding, implement them into this engine, create modules, designs, and other developments; they will be useful.</p>
         </div>
     </div>
     <div class="col-lg-4">
-    <div class="p-3 border rounded" style="background-color: #f8f9fa;">
+    <div class="p-3 border rounded" style="background-color: #f8f9fa; font-size: 1.1rem;">
         <h5 class="mb-3">Development: Uzhost Group</h5>
         <p>Github - <a href="https://github.com/uzhost/fastcore">Github/FastCore</a></p>
         <hr>
@@ -97,3 +98,4 @@ $stats = $db->query("SELECT * FROM db_stats WHERE id = '1'")->fetchArray();
 </div>
 
 </div>
+
