aec5346a3f61bb84232fc7953b66945af62df33a
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2024-05-11 19:45:53 +0500

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,38 +1,42 @@
-<?php if(!defined('FastCore')){ exit('Oops!'); }
+<?php
+if (!defined('FastCore')) {
+    exit('Oops!');
+}
 
 # База данных
-const dbHost = '45.81.226.183';
-const dbUser = 'pligin_fastcore';
-const dbPass = 'W{pJjqPk(ogL';
-const dbName = 'pligin_fastcore'; 
+const dbHost = 'localhost';
+const dbUser = 'db_user';
+const dbPass = 'db_pass';
+const dbName = 'db_name';
 
 # Подключение к БД
 include('classes/db.php');
 $db = new db(dbHost, dbUser, dbPass, dbName);
 
-class config {
+class config
+{
+    # Настройки сайта
+    public string $start_time = '1715363489';
+    public string $sitename = 'FASTCOIN'; // Название
+    public string $email = 'support@fastcoin.info'; // Почта
+    public string $email_domain = 'fastcore.info'; // domain. Eg: domain.com
 
-	# Настройки сайта
-	public $start_time = '1596834000';
-	public $sitename = 'FastCore v0.8'; // Название
-	public $email = 'support@fastcore.ml'; // Почта
+    # Админка
+    public string $adm_dir = 'admin'; // Директория
+    public string $adm_name = 'admin'; // Логин
+    public string $adm_pass = '123456'; // Пароль
 
-	# Админка 
-	public $adm_dir = 'admin'; // Директория
-	public $adm_name = 'admin'; // Логин
-	public $adm_pass = '123456'; // Пароль
-	
-	# PAYEER
-	public $py_shop = '1111'; // ID магазина
-	public $py_secret = '1111'; // SECRET ключ магазина
-	public $py_NUM = 'P8706145'; // Номер кошелька
-	public $py_apiID = '1635281052'; // API ID
-	public $py_apiKEY = '8cDT9O79gO4dR7I9';// API KEY
+    # PAYEER
+    public string $py_shop = '1111'; // ID магазина
+    public string $py_secret = '1111'; // SECRET ключ магазина
+    public string $py_NUM = 'P1234567'; // Номер кошелька
+    public string $py_apiID = '1234567890'; // API ID
+    public string $py_apiKEY = '9876543210'; // API KEY
 
-	# FREEKASSA
-	public $fk_id = '1111'; // ID магазина FK
-	public $fk_key = '1111'; // SECRET 1
-	public $fk_key2 = '2222'; // SECRET 2
+    # FREEKASSA
+    public string $fk_id = '1111'; // ID магазина FK
+    public string $fk_key = '1111'; // SECRET 1
+    public string $fk_key2 = '2222'; // SECRET 2
 }
 
 ?>
