427b4758e77489145cd3ca80df048393813e2f59
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2024-05-11 20:10:33 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,138 +1,139 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+
+define('Environment', 'development'); // development, production
+
+if (Environment === 'development') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+} else {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 0);
+}
 
 /***
- * Движок - FastCore v0.8 08.08.2020
- * Скрипт предназначен для свободного пользования.
- * Разработка и поддержка: Jumast & Kolyaka105 
- * Контакты: jumast@ya.ru - kolya105@ukr.net
- * Обновления тут:  https://vk.com/fastcore
+ * Engine - FastCore v0.9 08.08.2020
+ * The script is intended for free use.
+ * Development and support: Jumast & Kolyaka105
+ * Contacts: jumast@ya.ru - kolya105@ukr.net
+ * Updates here: https://vk.com/fastcore
+ * Improved Version Repository: https://github.com/rubensrocha/fastcore
  **/
 
-# Генерация страницы
+# Generate page
 define('GenTime', microtime(true));
 
-# Старт сессии
+# Start session
 session_start();
 
-# Старт буфера
+# Start buffer
 ob_start();
 
 # Default title
-$opt = array();
+$opt = [];
 
-# Константа для Include
-define('FastCore',true);
+# Constants for Include
+define('FastCore', true);
+define('FastCoreVersion', '0.8.1');
 
-# Система
-spl_autoload_register(function ($lfc) {
-    require 'core/' . $lfc . '.php';
+# System
+spl_autoload_register(static function ($className) {
+    require 'core/' . $className . '.php';
 });
 
-# Класс конфига
-$config = new config;
+require_once 'core/config.php'; // Include the file that connects to the database
 
-# Функции
-$func = new func;
+
+# Config class
+$config = new config();
+
+
+# Functions
+$func = new func();
 $func->getDomain();
 
-# Директория админки
+# Admin directory
 $adm = $config->adm_dir;
 
-# Ищем роуты
-require('routes.php');
+# Find routes
+require 'routes.php';
 
-# Подключаем роутер
+# Include router
 $pg = new router();
-$routed_file = $pg->classname;
+$routedFile = $pg->classname;
 
-# Пользователь
+# User
 $uid = $_SESSION['uid'] ?? '0';
 $login = $_SESSION['login'] ?? 'Guest';
 
-# ==========================
-#  Начало вывода страниц
-# ==========================
+# Outputting pages
+if (isset($pg->segment[0]) && !empty($pg->segment[0] === 'user')) {
 
-# Аккаунт
-if (!empty($pg->segment[0] === 'user')) {
+    # Authorized or not
+    if (isset($_SESSION['uid']) > 0) {
 
-	# Авторизованный или не
-	if(isset($_SESSION['uid']) > 0) {
+        # If authorized, search in the database
+        $user = $db->query('SELECT * FROM db_users WHERE id = ?', [$uid])->fetchArray();
 
-		# Если авторизованный ищем в БД
-		$user = $db->query('SELECT * FROM db_users WHERE id = ?',$uid)->fetchArray();
-	
-		require('inc/head.php');
+        require 'inc/head.php';
 
-	require('inc/menu.php'); // Меню аккаунта
-	echo '<div class="content"><div class="wrapper">';
-	require('inc/title.php'); // Заголовок
-	require('pages/user/'.$routed_file); // Страницы аккаунта
-	echo '</div></div><div class="clearfix"></div>'; // див контент
+        require 'inc/menu.php'; // Account menu
+        echo '<div class="content"><div class="wrapper">';
+        require 'inc/title.php'; // Title
+        require 'pages/user/' . $routedFile; // Account pages
+        echo '</div></div><div class="clearfix"></div>'; // content div
 
-		require('inc/foot.php');
-	}
-	else { header('Location: /'); return; }
+        require 'inc/foot.php';
+    } else {
+        header('Location: /');
+        return;
+    }
+} # Admin panel
+elseif (isset($pg->segment[0]) && !empty($pg->segment[0] === $adm) ?? $pg->segment[0] === $adm) {
+    if (isset($_SESSION["admin"])) {
+        require 'pages/' . $adm . '/inc/head.php';
+        require 'pages/' . $adm . '/inc/menu.php';
+        require 'pages/' . $adm . '/' . $routedFile;
+        require 'pages/' . $adm . '/inc/foot.php';
+    } else {
+        require 'pages/' . $adm . '/inc/head.php';
+        require 'pages/' . $adm . '/login.php'; // Admin login
+    }
 }
 
-
-# Админка
-elseif (!empty($pg->segment[0] === ''.$adm.'') ?? $pg->segment[0] === ''.$adm.'') {
-	if(isset($_SESSION["admin"])){
-		require('pages/'.$adm.'/inc/head.php');
-		require('pages/'.$adm.'/inc/menu.php');
-		require('pages/'.$adm.'/'.$routed_file);
-		require('pages/'.$adm.'/inc/foot.php');
-	}
-	else {
-		require('pages/'.$adm.'/inc/head.php');
-		require('pages/'.$adm.'/login.php'); // Вход в админку
-	}
-}
-
-# Серфинг IFRAME
+# Surfing IFRAME
 /*
 elseif (!empty($pg->segment[0] === 'link')) {
-	if(isset($_SESSION["uid"])){
-		require('inc/view.php');
-	}
+    if(isset($_SESSION["uid"])){
+        require 'inc/view.php';
+    }
 }
 */
 
-# Гостевая
+# Guest
 else {
-	require('inc/head.php');
-	require('pages/'.$routed_file);
-	require('inc/foot.php');
+    require 'inc/head.php';
+    require 'pages/' . $routedFile;
+    require 'inc/foot.php';
 }
 
-# ==========================
-# Конец вывода страниц
-# ==========================
+# Page generation end
+$genPage = round((microtime(true) - GenTime), 5);
 
-
-# Заносим контент в переменную
-$content = ob_get_contents();
-
-# Очищаем буфер
-ob_end_clean();
-
-# Генерация страницы конец
-$gen_page = round((microtime(true) - GenTime), 5);
-
-# Заменяем данные
-if (empty($pg->segment[0] === ''.$adm.'')) {  // off-admin
-$content = str_replace('{!TITLE!}',$opt['title'],$content); 
+# Replace data
+if ($pg->found) {
+    if (isset($pg->segment[0]) && empty($pg->segment[0] === $adm)) {  // off-admin
+        $content = str_replace('{!TITLE!}', $opt['title'], $content);
+    }
+    if (isset($pg->segment[0]) && empty($pg->segment[0] === 'user') && empty($pg->segment[0] === $adm)) { // off-account
+        $content = str_replace(array('{!DESCRIPTION!}', '{!KEYWORDS!}'), array($opt['description'], $opt['keywords']), $content);
+    }
+} else {
+    $content = str_replace(array('{!TITLE!}', '{!DESCRIPTION!}', '{!KEYWORDS!}'), array('Page not found', '', ''), $content);
 }
-if (empty($pg->segment[0] === 'user') && empty($pg->segment[0] === ''.$adm.'')) { // off-account
-$content = str_replace('{!DESCRIPTION!}',$opt['description'],$content);
-$content = str_replace('{!KEYWORDS!}',$opt['keywords'],$content); 
-}
-$content = str_replace('{!GEN_PAGE!}', sprintf("%.5f", ($gen_page)) ,$content);
 
-# Выводим контент
+$content = str_replace('{!GEN_PAGE!}', sprintf("%.5f", ($genPage)), $content);
+
+# Output content
 echo $content;
-?>
